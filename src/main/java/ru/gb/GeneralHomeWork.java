e0f0b91cbdcfae6d8f6cbfcdb6d0f39bdb57cdcd
danfilof <danila.anfilofyev@gmail.com>
2021-10-19 23:36:00 +0200

    Second and third tasks done.

--- src/main/java/ru/gb/GeneralHomeWork.java
+++ src/main/java/ru/gb/GeneralHomeWork.java
@@ -6,11 +6,18 @@ public class GeneralHomeWork  {
 
     public static void main(String[] args)  {
     oneToZero();
+    oneToHundred();
+    timestwo();
+
+
+
+
+
     }
 
     public static void oneToZero() {
 
-        int[] arr = new int[] { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
         for (int i = 0; i < arr.length; i++) {
 
@@ -24,8 +31,33 @@ public class GeneralHomeWork  {
             }
 
         }
-        System.out.println (Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));
     }
 
+    public static void oneToHundred() {
+
+        int[] arr01 = new int[101];
+
+        for (int i = 0; i < arr01.length; i++) {
+
+            arr01[i] = i;
+        }
+        System.out.println(Arrays.toString(arr01));
+    }
+
+
+    public static void timestwo () {
+
+        int [] arr02 = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+
+        for (int i = 0; i < arr02.length; i++) {
+
+            if (arr02[i] < 6) {
+
+                arr02[i] = arr02[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(arr02));
+    }
 }
 
