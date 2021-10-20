836cd102d93b1b3d14dcd0245c51e29a663f7c01
danfilof <danila.anfilofyev@gmail.com>
2021-10-20 21:20:17 +0200

    5th task done.

--- src/main/java/ru/gb/GeneralHomeWork.java
+++ src/main/java/ru/gb/GeneralHomeWork.java
@@ -2,21 +2,21 @@ package ru.gb;
 
 import java.util.Arrays;
 
-public class GeneralHomeWork  {
+public class GeneralHomeWork {
 
-    public static void main(String[] args)  {
-    oneToZero();
-    oneToHundred();
-    timesTwo();
-    diagonAlley();
-
+    public static void main(String[] args) {
+        oneToZero();
+        oneToHundred();
+        timesTwo();
+        diagonAlley();
+        twoArguements(10, 25);
 
 
     }
 
     public static void oneToZero() {
 
-        int[] arr = new int[] {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
 
         for (int i = 0; i < arr.length; i++) {
 
@@ -45,9 +45,9 @@ public class GeneralHomeWork  {
     }
 
 
-    public static void timesTwo () {
+    public static void timesTwo() {
 
-        int [] arr02 = new int[] {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        int[] arr02 = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
 
         for (int i = 0; i < arr02.length; i++) {
 
@@ -59,24 +59,39 @@ public class GeneralHomeWork  {
         System.out.println(Arrays.toString(arr02));
     }
 
-public static void diagonAlley() {
+    public static void diagonAlley() {
 
-int [] [] arr03 = new int [6] [6];
+        int[][] arr03 = new int[6][6];
 
-for (int i  = 0; i < 6; i++) {
+        for (int i = 0; i < 6; i++) {
 
-    for (int j = 0; j < 6; j++) {
+            for (int j = 0; j < 6; j++) {
 
-        if (i == j) {
+                if (i == j) {
 
-            arr03[i][j] = 1;
+                    arr03[i][j] = 1;
+
+                }
+                System.out.printf("%2d", arr03[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int[] twoArguements(int len, int initialValue) {
+
+        int[] arr04 = new int[len];
+
+        for (int i = 0; i < arr04.length; i++) {
+
+            arr04[i] = initialValue;
 
         }
-        System.out.printf("%2d" , arr03[i][j]);
-    }
-System.out.println();
-}
 
-}
+        System.out.println(Arrays.toString(arr04));
+        return arr04;
+    }
+
 }
 
