76d7ec918238569d4482bb5e04d69097511b3de0
danfilof <danila.anfilofyev@gmail.com>
2021-10-26 01:41:04 +0200

    The constructor fills the fields

--- src/main/java/ru/gb/Employee.java
+++ src/main/java/ru/gb/Employee.java
@@ -8,4 +8,17 @@ public class Employee {
     private String phoneNumber;
     private String salary;
     private int age;
+
+
+
+    public Employee (String name, String position, String email, String phoneNumber, String salary, int age ) {
+
+        name = "Maria Petrova";
+        position = "teacher";
+        email = "maria.petrova@gmail.com";
+        phoneNumber = "89111333333";
+        salary = "120";
+        age = 33;
+
+    }
 }
