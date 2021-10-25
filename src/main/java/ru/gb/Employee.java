05f4148f885f21abe0cc939b5824af1e982de604
danfilof <danila.anfilofyev@gmail.com>
2021-10-26 01:33:16 +0200

    First task done, class and fields created

---
+++ src/main/java/ru/gb/Employee.java
@@ -0,0 +1,11 @@
+package ru.gb;
+
+public class Employee {
+
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private String salary;
+    private int age;
+}
