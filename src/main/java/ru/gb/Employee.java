dcdd7dd28878511b532dbebc63d06f696ecfec21
danfilof <danila.anfilofyev@gmail.com>
2021-10-26 02:31:14 +0200

    Information in console

--- src/main/java/ru/gb/Employee.java
+++ src/main/java/ru/gb/Employee.java
@@ -1,6 +1,10 @@
 package ru.gb;
 
-public class Employee {
+class Employee{
+
+    public static void main(String[] args) {
+
+    }
 
     private String name;
     private String position;
@@ -10,15 +14,20 @@ public class Employee {
     private int age;
 
 
-
     public Employee (String name, String position, String email, String phoneNumber, String salary, int age ) {
 
         name = "Maria Petrova";
-        position = "teacher";
+        position = "Math teacher";
         email = "maria.petrova@gmail.com";
         phoneNumber = "89111333333";
         salary = "120";
         age = 33;
+    }
 
+      void print() {
+
+        System.out.println("Name: " + name + "Position: " + position + "Email: " + email + "Phone number: " + phoneNumber + "Salary: " + salary + "Age: " + age);
     }
 }
+
+
