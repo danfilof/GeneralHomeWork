f3eaade6a13b8c6d51a4776d5efc6a47f536c05c
danfilof <danila.anfilofyev@gmail.com>
2021-10-31 17:23:44 +0100

    Classes cat and dog created

--- src/main/java/ru/gb/Animals.java
+++ src/main/java/ru/gb/Animals.java
@@ -1,4 +1,25 @@
 package ru.gb;
 
 public class Animals {
+
+    public Animals() {
+
+    }
+
+    public void run() {
+        System.out.println("Животное пробежало");
+    }
+
+    public void swim() {
+        System.out.println("Животное проплыло");
+    }
+
+}
+
+ class cat extends Animals {
+
+}
+
+class dog extends Animals {
+
 }
