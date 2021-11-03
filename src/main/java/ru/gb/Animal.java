ba5724469cc4765e1a70ee609fd65fae3ffb653e
danfilof <danila.anfilofyev@gmail.com>
2021-11-03 13:28:22 +0100

    Main task solved with getters and setters. Only *task left to do

--- src/main/java/ru/gb/Animal.java
+++ src/main/java/ru/gb/Animal.java
@@ -5,12 +5,12 @@ class MainClass {
     public static void main(String[] args) {
 
         cat cat1 = new cat();
-        cat1.SetName("Murka");
-        System.out.println(cat1.GetName());
+        cat1.setName("Murka");
+        System.out.println(cat1.getName());
 
         dog dog1 = new dog();
-        dog1.SetName("Sharik");
-        System.out.println(dog1.GetName());
+        dog1.setName("Sharik");
+        System.out.println(dog1.getName());
 
         cat1.swim();
         cat1.run();
@@ -19,18 +19,17 @@ class MainClass {
         dog1.run();
 
 
-        // Why the hell those variables are visible for MAIN?!?
-        cat1.name = "Barsik";
-        System.out.println(cat1.GetName());
-        dog1.name = "Bobik";
-        System.out.println(dog1.GetName());
+       // cat1.name = "Barsik";
+        System.out.println(cat1.getName());
+        //dog1.name = "Bobik";
+        System.out.println(dog1.getName());
     }
 }
 
 public class Animal {
-    protected int distance = 199;
-    protected String name;
-
+    private int distance = 199;
+    private String name;
+    
     public Animal() {
     }
 
@@ -42,34 +41,38 @@ public class Animal {
         System.out.println("Животное проплыло");
     }
 
-    public String GetName() {
+    public String getName() {
         return name;
     }
 
-    public void SetName(String NewName) {
+    public void setName(String NewName) {
         name = NewName;
     }
+
+    public int getDistance() { return distance;  }
 }
 
 class cat extends Animal {
+
+
     @Override
     public void run() {
-        if (distance <= 200) {
-            System.out.println("Cat " + name + " пробежал " + distance + " метров");
-        } else if (distance < 0) {
+        if (getDistance() <= 200 && getDistance() >= 0) {
+            System.out.println("Cat " + getName() + " пробежал " + getDistance() + " метров");
+        } else if (getDistance() < 0) {
             System.out.println("Введите положительное значение");
         } else {
-            System.out.println("Cat " + name + " не может столько пробежать");
+            System.out.println("Cat " + getName() + " не может столько пробежать");
         }
 
     }
     @Override
     public final void swim() {
 
-        if (distance >= 0) {
-            System.out.println("Cat " + name + " не умеет плавать");
+        if (getDistance() >= 0) {
+            System.out.println("Cat " + getName() + " не умеет плавать");
         } else {
-            System.out.println("Cat " + name + " не умеет плавать");
+            System.out.println("Cat " + getName() + " не умеет плавать");
         }
     }
 
@@ -79,23 +82,23 @@ class dog extends Animal {
     @Override
     public void run() {
 
-        if (distance <= 500) {
-            System.out.println("Dog " + name + " пробежал " + distance + " метров");
-        } else if (distance < 0) {
+        if (getDistance() <= 500 && getDistance() >= 0) {
+            System.out.println("Dog " + getName() + " пробежал " + getDistance() + " метров");
+        } else if (getDistance() < 0) {
             System.out.println("Введите положительное значение");
         } else {
-            System.out.println("Dog " + name + " не может столько пробежать");
+            System.out.println("Dog " + getName() + " не может столько пробежать");
         }
 
     }
     @Override
     public void swim() {
-        if (distance <= 10) {
-            System.out.println("Dog " + name + " проплыл " + distance + " метров");
-        } else if (distance < 0) {
+        if (getDistance() <= 10) {
+            System.out.println("Dog " + getName() + " проплыл " + getDistance() + " метров");
+        } else if (getDistance() < 0) {
             System.out.println("Введите положительное значение");
         } else {
-            System.out.println("Dog " + name + " не может столько проплыть");
+            System.out.println("Dog " + getName() + " не может столько проплыть");
         }
 
     }
