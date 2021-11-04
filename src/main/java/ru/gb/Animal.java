e50e7b781d8203d1d89c1273d118199c9200066f
danfilof <danila.anfilofyev@gmail.com>
2021-11-04 13:21:19 +0100

    Add comments

--- src/main/java/ru/gb/Animal.java
+++ src/main/java/ru/gb/Animal.java
@@ -4,15 +4,15 @@ class MainClass {
 
     public static void main(String[] args) {
 
-        cat cat1 = new cat();
+        cat cat1 = new cat(); // создаем объект cat1, с помощью сеттера даем емя имя Мурка и печатаем результат в консоль
         cat1.setName("Murka");
         System.out.println(cat1.getName());
 
-        dog dog1 = new dog();
+        dog dog1 = new dog(); // создаем объект dog1, с помощью сеттера даем емя имя Мурка и печатаем результат в консоль
         dog1.setName("Sharik");
         System.out.println(dog1.getName());
 
-        cat1.swim();
+        cat1.swim(); // Вызываем все рабочие методы
         cat1.run();
 
         dog1.swim();
@@ -22,35 +22,33 @@ class MainClass {
 }
 
 public class Animal {
-    private int distance = -5;
+    private int distance = 8; // Создаем 2 приватных поля класса - дистанцию и имя
     private String name;
     
-    public Animal() {
+    public Animal() { // Пустой конструктор, он в любом случае априори создается, даже если мы его не прописывали
     }
 
     public void run() {
         System.out.println("Животное пробежало");
-    }
-
+    } // Метод БЕЖАТЬ
+                                                                                         // Все животные это умеют
     public void swim() {
         System.out.println("Животное проплыло");
-    }
+    } // Метод ПЛЫТЬ
 
     public String getName() {
         return name;
-    }
+    } // Геттер для получения имени вне класса
 
     public void setName(String NewName) {
         name = NewName;
-    }
+    } // Сеттер для изменения имени вне класса
 
-    public int getDistance() { return distance;  }
+    public int getDistance() { return distance;  } // Геттер для получения дистанции вне класса
 }
 
 class cat extends Animal {
-
-
-    @Override
+    @Override // Переопределение метода БЕЖАТЬ под условия, поставленные для кота, и вывод результата в консоль
     public void run() {
         if (getDistance() <= 200 && getDistance() >= 0) {
             System.out.println("Cat " + getName() + " пробежал " + getDistance() + " метров");
@@ -61,7 +59,7 @@ class cat extends Animal {
         }
 
     }
-    @Override
+    @Override // Переопределение метода ПЛЫТЬ, поставленного для кота, и вывод результата в консоль
     public final void swim() {
 
         if (getDistance() >= 0) {
@@ -74,7 +72,7 @@ class cat extends Animal {
 }
 
 class dog extends Animal {
-    @Override
+    @Override // Переопределение метода БЕЖАТЬ под условия, поставленные для пса, и вывод результата в консоль
     public void run() {
 
         if (getDistance() <= 500 && getDistance() >= 0) {
@@ -86,7 +84,7 @@ class dog extends Animal {
         }
 
     }
-    @Override
+    @Override // Переопределение метода ПЛЫТЬ под условия, поставленные для пса, и вывод результата в консоль
     public void swim() {
         if (getDistance() <= 10 && getDistance() >= 0 ) {
             System.out.println("Dog " + getName() + " проплыл " + getDistance() + " метров");
